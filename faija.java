332a26d1498e633e715e66ea3e750b582e3ec54c
Tasfia-faija <tasfia719@gmail.com>
2022-05-16 18:55:16 +0600

    java file created in master

---
+++ faija.java
@@ -0,0 +1,22 @@
+class new{
+    String name;
+    int number;
+    public void set_name(String a){
+        name=a;
+    }
+    public void set_number(int n){
+        number=n;
+    }
+    public string get_name(){
+        return name;
+    }
+    public int get_number(){
+        return number;
+    }
+
+new(String a1, int num){
+    name=a1;
+    number=num;
+}
+
+};
