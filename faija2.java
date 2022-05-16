e18cd7e32953835ce86d4be486ce8cb1b6c60776
Tasfia-faija <tasfia719@gmail.com>
2022-05-16 18:59:05 +0600

    java file reated in feature branch

---
+++ faija2.java
@@ -0,0 +1,15 @@
+class A{
+    int a;
+    int b;
+    public void set_a(int x)
+    { a=x; }
+    public void set_b(int y){
+        b=y;
+    }
+    public int get_a(){
+        return a;
+    }
+    public int get_b(){
+        return b;
+    }
+};
